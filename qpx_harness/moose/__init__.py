0fe36777e648f5748c5629843d31595aab545fe9
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-08-30 23:01:29 +0100

    Export reusable MOOSE block primitives

--- qpx_harness/moose/__init__.py
+++ qpx_harness/moose/__init__.py
@@ -1,5 +1,14 @@
 """Reusable MOOSE input operations."""
 
+from .blocks import (
+    MooseBlockError,
+    append_top_level_block,
+    has_block,
+    insert_child_block,
+    remove_block,
+    replace_block,
+    require_absent,
+)
 from .parameters import (
     MooseParameterError,
     direct_children,
@@ -11,10 +20,17 @@ from .parameters import (
 )
 
 __all__ = [
+    "MooseBlockError",
     "MooseParameterError",
+    "append_top_level_block",
     "direct_children",
     "get_parameter",
+    "has_block",
+    "insert_child_block",
     "parameter_count",
+    "remove_block",
+    "replace_block",
+    "require_absent",
     "unquote",
     "upsert_parameter",
     "words",
