cd98dea6a3b7334c3f6b84f9f26633ca08abe410
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-08-31 11:51:06 +0100

    Issue48 cleanup: remove stale v2 import from coupling diagnostic

--- qpx_harness/fast_plasma_coupling_diagnostic.py
+++ qpx_harness/fast_plasma_coupling_diagnostic.py
@@ -18,7 +18,6 @@ from typing import Any
 
 from . import evidence
 from . import execution_contract as ec
-from . import fast_plasma_relaxation_v2 as v2
 from . import fast_plasma_relaxation_v5 as v5
 from . import output_observation_contract as ooc
 from .moose_input import MooseInput, MooseInputError
