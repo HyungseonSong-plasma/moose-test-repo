cacdeb153eb000c2f14095489227b95baa52ff63
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-08-31 09:57:25 +0100

    Issue48 cleanup: retire legacy transport probe from inventory

--- tests/Issue48_qpx_harness_generality/cleanup_inventory.py
+++ tests/Issue48_qpx_harness_generality/cleanup_inventory.py
@@ -33,7 +33,6 @@ CLEANUP_CANDIDATES = (
     "performance_core",
     "performance_investigation",
     "performance_smoke",
-    "performance_transport_probe",
     "performance_transport_probe_direct",
     "scale_audit",
 )
