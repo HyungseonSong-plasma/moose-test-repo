842e8af15d4d0ce5b6fd15bb982e062a1913c7d4
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-09-01 08:08:26 +0100

    Issue48 cleanup: point parser characterization at coupling semantic owner

--- tests/Issue48_qpx_harness_generality/wp2_analysis_characterization.py
+++ tests/Issue48_qpx_harness_generality/wp2_analysis_characterization.py
@@ -9,7 +9,7 @@ ROOT = Path(__file__).resolve().parents[2]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
-from qpx_harness import fast_plasma_coupling_diagnostic as issue43
+from qpx_harness import issue43_coupling_diagnostic as issue43
 from qpx_harness.moose import log as moose_log
 from qpx_harness.petsc import jacobian as petsc_jacobian
 from qpx_harness.petsc import ksp as petsc_ksp
