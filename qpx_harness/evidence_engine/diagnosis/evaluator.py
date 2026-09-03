6737ea3bf8cd88557237621d52975c31b9958198
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-09-03 11:59:55 +0100

    Retire transitional diagnosis evaluator facade

--- qpx_harness/evidence_engine/diagnosis/evaluator.py
+++
@@ -1,3 +0,0 @@
-"""Transitional facade for :mod:`qpx_harness.diagnose.evaluator`."""
-from qpx_harness.diagnose.evaluator import *  # noqa: F401,F403
-from qpx_harness.diagnose.evaluator import __all__
