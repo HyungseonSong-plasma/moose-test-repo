72cfcfc1167de2988eaa373d169bbeb2400e4c9a
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-09-03 09:34:23 +0100

    Export dynamic evidence schema API

--- qpx_harness/evidence_engine/__init__.py
+++ qpx_harness/evidence_engine/__init__.py
@@ -7,18 +7,34 @@ from .diagnose import (
     summarize_constant_state,
     summarize_constant_state_report,
 )
-from .schema import CELL_KEY_COLUMNS, FACE_REQUIRED_COLUMNS, require_columns
+from .schema import (
+    CELL_KEY_COLUMNS,
+    DEFAULT_FACE_CONTRACT,
+    FACE_REQUIRED_COLUMNS,
+    ColumnRole,
+    ColumnSpec,
+    DynamicSchemaContract,
+    SchemaResolution,
+    normalize_and_project,
+    require_columns,
+)
 from .store import EvidenceStore
 from .transform import build_cell_evidence, prepare_face_evidence, write_evidence_bundle
 
 __all__ = [
     "CELL_KEY_COLUMNS",
+    "DEFAULT_FACE_CONTRACT",
     "FACE_REQUIRED_COLUMNS",
+    "ColumnRole",
+    "ColumnSpec",
     "DiagnosisReport",
+    "DynamicSchemaContract",
     "EvidenceStore",
     "EvidenceTolerances",
     "FailureLocation",
+    "SchemaResolution",
     "build_cell_evidence",
+    "normalize_and_project",
     "prepare_face_evidence",
     "require_columns",
     "summarize_constant_state",
