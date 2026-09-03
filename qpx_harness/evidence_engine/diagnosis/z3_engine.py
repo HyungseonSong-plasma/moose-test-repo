a79bd958ac132c0a2720807e55effc7631bc33d1
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-09-03 12:00:23 +0100

    Retire transitional diagnosis Z3 facade

--- qpx_harness/evidence_engine/diagnosis/z3_engine.py
+++
@@ -1,3 +0,0 @@
-"""Transitional facade for :mod:`qpx_harness.diagnose.z3_engine`."""
-from qpx_harness.diagnose.z3_engine import *  # noqa: F401,F403
-from qpx_harness.diagnose.z3_engine import __all__
