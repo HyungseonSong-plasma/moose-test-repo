2677fba2c8f3fb8bf20db4e40102d545dfdab730
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-09-03 11:57:48 +0100

    Retire transitional evidence engine facade

--- qpx_harness/evidence_engine/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Transitional compatibility facade over canonical Evidence and Diagnose."""
-
-from qpx_harness.evidence import *  # noqa: F401,F403
-from qpx_harness.evidence import __all__ as _evidence_all
-from qpx_harness.diagnose import *  # noqa: F401,F403
-from qpx_harness.diagnose import __all__ as _diagnose_all
-
-__all__ = list(dict.fromkeys((*_evidence_all, *_diagnose_all)))
