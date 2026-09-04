963e538b2b948a3c9ea4009d1c24c9164056d2e4
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-09-04 08:56:47 +0100

    Register Issue 26 E1 experiment protocol

--- qpx_harness/application/experiment_registry.py
+++ qpx_harness/application/experiment_registry.py
@@ -13,6 +13,7 @@ from .experiment_spec import ExperimentSpec
 ExperimentRunner = Callable[[ExperimentSpec], int]
 
 _PROTOCOLS: dict[str, str] = {
+    "issue26-electron-energy-e1": "qpx_harness.application.protocols.issue26_electron_energy_e1:run_protocol",
     "issue27-surface-reaction-controlled-wall": "qpx_harness.application.protocols.issue27_surface_reaction_controlled_wall:run_protocol",
     "r3-electron-master-diagnostic": "qpx_harness.application.protocols.r3_electron_master_diagnostic:run_protocol",
     "r3-electron-scaling-counterfactual": "qpx_harness.application.protocols.r3_electron_scaling_counterfactual:run_protocol",
