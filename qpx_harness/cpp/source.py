254700765e59dcfb678a4aa42e804da3945efddf
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-09-04 23:38:17 +0100

    refactor: retire generic C++ compatibility facade

--- qpx_harness/cpp/source.py
+++
@@ -1,8 +0,0 @@
-"""Deprecated compatibility facade for generic C++ source observation.
-
-Canonical owner: :mod:`qpx_harness.observation.source_code.cpp`.
-No new semantic responsibility may be added here.
-"""
-from qpx_harness.observation.source_code.cpp import CppSource, CppSourceError, Span, mask_cpp
-
-__all__ = ["CppSource", "CppSourceError", "Span", "mask_cpp"]
