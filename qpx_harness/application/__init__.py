bc979f4a2eb2b26472be87c9e28476920ebcdd69
HyungseonSong-plasma <hssongplasma@gmail.com>
2026-09-06 02:21:43 +0100

    refactor: retire legacy experiment application exports

--- qpx_harness/application/__init__.py
+++ qpx_harness/application/__init__.py
@@ -1,7 +1,5 @@
 """Canonical application services composing QPX subsystem public APIs."""
 
-from .experiment_service import run_experiment, validate_experiment_spec
-from .experiment_spec import CaseSource, ExperimentControl, load_experiment_spec
 from .operations import (
     analyze_green_gauss,
     diagnose_constant_green_gauss,
@@ -10,13 +8,8 @@ from .operations import (
 )
 
 __all__ = [
-    "CaseSource",
-    "ExperimentControl",
     "analyze_green_gauss",
     "diagnose_constant_green_gauss",
-    "load_experiment_spec",
     "normalize_temporal_run_csv",
     "preflight_input",
-    "run_experiment",
-    "validate_experiment_spec",
 ]
