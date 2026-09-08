f4b4c049804944b468cfa2619a47a7e6bdc571cb
raven <hssongplasma@gmail.com>
2026-09-08 19:27:16 +0100

    refactor: export Physics runtime API

--- physics_harness/execution/__init__.py
+++ physics_harness/execution/__init__.py
@@ -3,7 +3,7 @@
 from .cases import CaseError, stage_case, validate_case_references
 from .compiler import UnresolvedPolicyError, compile_execution_plan
 from .plan import ExecutionCase, ExecutionPlan
-from .runtime import RunResult, TelemetrySample, resolve_executable, run_command, run_qpx, validate_executable
+from .runtime import RunResult, TelemetrySample, resolve_executable, run_command, run_physics, validate_executable
 from .workspace import discover_manifests, load_manifest, manifest_type
 
 __all__ = [
@@ -19,7 +19,7 @@ __all__ = [
     "manifest_type",
     "resolve_executable",
     "run_command",
-    "run_qpx",
+    "run_physics",
     "stage_case",
     "validate_case_references",
     "validate_executable",
