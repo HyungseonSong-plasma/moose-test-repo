68a456aa854c173a77337cbe7ebd5ff33bfe8ce0
raven <hssongplasma@gmail.com>
2026-09-14 18:51:33 +0100

    Add Issue 228 diffusion face diagnostic

---
+++ physics_app/include/fvkernels/PhysicsFVDiffusionDiagnostic.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "FVDiffusion.h"
+
+#include <vector>
+
+/** Diagnostic-only wrapper around framework FVDiffusion for Issue #228. */
+class PhysicsFVDiffusionDiagnostic : public FVDiffusion
+{
+public:
+  static InputParameters validParams();
+  PhysicsFVDiffusionDiagnostic(const InputParameters & params);
+
+protected:
+  ADReal computeQpResidual() override;
+  bool probeFace() const;
+
+  const std::vector<unsigned int> _diagnostic_element_ids;
+};
