72cc996e1e19432d4770392ca7d038c669d06060
raven <hssongplasma@gmail.com>
2026-09-24 12:58:25 +0100

    Issue 310 Gen19: carry delta-phi convergence class

---
+++ physics_app/include/convergence/PhysicsDeltaPhiMultiAppConvergence.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "DefaultMultiAppFixedPointConvergence.h"
+
+/**
+ * MultiApp fixed-point convergence that requires BOTH:
+ *   1. the standard MOOSE fixed-point residual criterion; and
+ *   2. max |phi^(k)-phi^(k-1)| <= delta_phi_abs_tol.
+ *
+ * The delta-phi quantity is supplied by a postprocessor executed on
+ * MULTIAPP_FIXED_POINT_CONVERGENCE.
+ */
+class PhysicsDeltaPhiMultiAppConvergence : public DefaultMultiAppFixedPointConvergence
+{
+public:
+  static InputParameters validParams();
+  PhysicsDeltaPhiMultiAppConvergence(const InputParameters & parameters);
+
+  MooseConvergenceStatus checkConvergence(unsigned int n_iter) override;
+
+protected:
+  const PostprocessorValue & _delta_phi;
+  const Real _delta_phi_abs_tol;
+};
